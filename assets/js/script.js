246f0f20f7e5dddcef8e6f070b6d213abc4a77d6
MarcosGaiotto <marcosgaiotto@hotmail.com>
2021-02-21 22:04:40 -0300

    desktop layout added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,24 +12,30 @@ let ipInfo = {
     isp: ""
 }
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
-
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-
+let ipAddress = document.getElementById("ip-address");
+let locationCity = document.getElementById("location");
+let timezone = document.getElementById("timezone");
+let isp = document.getElementById("isp");
 
 getIp().then(() => {
     getIpInfo(ipInfoUrl, ipInfo.ip).then(() => {
-        console.log(ipInfo);
+        ipAddress.innerHTML = ipInfo.ip;
+        locationCity.innerHTML = ipInfo.region + ', ' + ipInfo.city;
+        timezone.innerHTML = ipInfo.timeZone;
+        isp.innerHTML = ipInfo.isp;
+        mymap = L.map('mapid').setView([ipInfo.latitude, ipInfo.longitude], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(mymap);
+        marker = L.marker([ipInfo.latitude, ipInfo.longitude]).addTo(mymap);
+        let markerIcon = document.getElementsByClassName("leaflet-marker-icon")[0];
+        markerIcon.src = "./assets/images/icon-location.svg";
+        markerIcon.style.width = "30px";
     });
 });
 
